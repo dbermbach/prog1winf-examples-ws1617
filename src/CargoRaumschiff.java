55788ae0e17b92a9e5f1050da5eb47f419283622
dbermbach <dbermbach@gmail.com>
2016-11-28 13:45:45 +0100

    examples Nov 28

---
+++ src/CargoRaumschiff.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Dave
+ *
+ */
+public class CargoRaumschiff extends Raumschiff {
+
+	private double laderaumVolumen;
+
+	public static void main(String[] args) {
+		CargoRaumschiff cr = new CargoRaumschiff();
+		cr.fly(1000);
+	}
+
+}
---
+++ src/Raumschiff.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Dave
+ *
+ */
+public class Raumschiff {
+	
+	private double length;
+
+	public void fly(double speed) {
+		System.out.println("Flying with speed " + speed);
+	}
+
+	/**
+	 * @return the length
+	 */
+	public double getLength() {
+		return this.length;
+	}
+
+	/**
+	 * @param length the length to set
+	 */
+	public void setLength(double length) {
+		this.length = length;
+	}
+	
+
+}
