e91f2a7c385d7e1227d8b1d96fbb8cb02abf3920
dbermbach <dbermbach@gmail.com>
2017-01-23 15:52:42 +0100

    added example programs (data structures, concurrency)

---
+++ src/de/tuberlin/ise/prog1winf/btree/BTreeNode.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package de.tuberlin.ise.prog1winf.btree;
+
+/**
+ * @author Dave
+ *
+ */
+public class BTreeNode {
+
+	public int value;
+
+	public BTreeNode left;
+
+	public BTreeNode right;
+
+	/**
+	 * @param value
+	 */
+	public BTreeNode(int value) {
+		super();
+		this.value = value;
+	}
+
+}
